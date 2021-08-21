2a3a193df1fda2956c7fcce95e9d6f180a3a92a7
ban vuong <vuongdieuban@gmail.com>
2021-08-21 16:47:18 -0600

    create EventsGateway to handle websocket connect and disconnect

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionEntity } from './database/entities/Transaction.entity';
+import { EventsGateway } from './events.gateway';
 import { typeormConfigOptions } from './ormconfig';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeormConfigOptions), TypeOrmModule.forFeature([TransactionEntity])],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EventsGateway],
 })
 export class AppModule {}
---
+++ backend/src/events.gateway.ts
@@ -0,0 +1,72 @@
+import { Logger } from '@nestjs/common';
+import { WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  private readonly logger: Logger = new Logger('EventsGateway');
+
+  private readonly connectedSockets = new Map<string, Socket[]>();
+
+  constructor() {
+    // this.applicationEventsPublisher.data$.subscribe(data => {
+    //   const sockets = this.connectedSockets.get(data.payload.userId);
+    //   if (!sockets) {
+    //     return;
+    //   }
+    //   sockets.forEach(socket => socket.emit(data.event, data));
+    // });
+  }
+
+  public handleDisconnect(socket: Socket) {
+    try {
+      const clientId = socket.handshake.query['clientId'] as string;
+      if (!clientId) {
+        throw new Error('ClientId is required');
+      }
+      this.removeSocketFromClient(clientId, socket);
+    } catch (err) {
+      this.processWSError(err, socket);
+    }
+  }
+
+  async handleConnection(socket: Socket) {
+    try {
+      const clientId = socket.handshake.query['clientId'] as string;
+      if (!clientId) {
+        throw new Error('ClientId is required');
+      }
+      this.addSocketToClient(clientId, socket);
+    } catch (err) {
+      this.processWSError(err, socket);
+    }
+  }
+
+  private addSocketToClient(clientId: string, socket: Socket) {
+    const clientSockets = this.connectedSockets.get(clientId);
+    if (clientSockets) {
+      clientSockets.push(socket);
+    } else {
+      this.connectedSockets.set(clientId, [socket]);
+    }
+  }
+
+  private removeSocketFromClient(clientId: string, socket: Socket) {
+    const clientSockets = this.connectedSockets.get(clientId);
+    if (!(clientSockets && clientSockets.length)) {
+      return;
+    }
+
+    const disconnectSocketId = socket.id;
+    const updatedClientSockets = clientSockets.filter(s => s.id !== disconnectSocketId);
+    this.connectedSockets.set(clientId, updatedClientSockets);
+  }
+
+  private processWSError(err: Error, socket: Socket) {
+    socket.emit('exception', {
+      status: 'Error',
+      errorType: err.name,
+      message: err.message,
+    });
+  }
+}
