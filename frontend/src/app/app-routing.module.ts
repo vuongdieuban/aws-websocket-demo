7e099b9c575b15d952f078b2957e03dacdb75224
ban vuong <vuongdieuban@gmail.com>
2021-08-21 18:37:03 -0600

    add router and placeholder components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AsyncComponent } from './pages/async/async.component';
+import { HelloComponent } from './pages/hello/hello.component';
+import { SyncComponent } from './pages/sync/sync.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tx/sync', component: SyncComponent },
+  { path: 'tx/async', component: AsyncComponent },
+  { path: 'hello', component: HelloComponent },
+  { path: '', redirectTo: '/hello', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsyncComponent } from './pages/async/async.component';
+import { HelloComponent } from './pages/hello/hello.component';
+import { SyncComponent } from './pages/sync/sync.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SyncComponent, AsyncComponent, HelloComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSidenavModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
