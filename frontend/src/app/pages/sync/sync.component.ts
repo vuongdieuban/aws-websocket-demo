579e01efd42d6abdaa29bd2c1f5a36eb1d7bd2bb
ban vuong <vuongdieuban@gmail.com>
2021-08-23 22:03:29 -0600

    aws socket api gatway

--- frontend/src/app/pages/sync/sync.component.ts
+++ frontend/src/app/pages/sync/sync.component.ts
@@ -16,6 +16,18 @@ export class SyncComponent implements OnInit, OnDestroy {
   constructor(private readonly txService: TransactionsService) {}
 
   async ngOnInit() {
+    const socket = new WebSocket('wss://g7gdtdvrwg.execute-api.ca-central-1.amazonaws.com/dev');
+
+    // Connection opened
+    socket.addEventListener('open', function (event) {
+      socket.send(JSON.stringify({ action: 'getTx', data: 'LOL' }));
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', function (event) {
+      console.log('Message from server ', event.data);
+    });
+
     const data = await this.txService.getTxSync().pipe(take(1)).toPromise();
     this.txData = data;
     console.log('SyncData', data);
